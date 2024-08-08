1648e7bd02ad1addffe877f5c4924f882eab064c
Chris Holder <chrisholder987@hotmail.com>
2024-08-08 17:36:54 +0200

    merge ksc

--- tsml_eval/estimators/clustering/__init__.py
+++ tsml_eval/estimators/clustering/__init__.py
@@ -3,7 +3,9 @@
 __all__ = [
     "SklearnToTsmlClusterer",
     "RClustering",
+    "KSpectralCentroid",
 ]
 
 from tsml_eval.estimators.clustering._r_clustering import RClustering
 from tsml_eval.estimators.clustering._sklearn_clusterer import SklearnToTsmlClusterer
+from tsml_eval.estimators.clustering.ksc._k_spectral_centroid import KSpectralCentroid
