fixed ksc average

tsml_eval/estimators/clustering/ksc/_shift_invariant_average.py
@@ -1,7 +1,6 @@
 """Shift-invariant average."""
 
 import numpy as np
-from numpy.linalg import eigh, norm
 
 from tsml_eval.estimators.clustering.ksc._shift_invariant_distance import (
     shift_invariant_best_shift,
@@ -11,6 +10,7 @@ from tsml_eval.estimators.clustering.ksc._shift_invariant_distance import (
 def shift_invariant_average(
     X: np.ndarray, initial_center: np.ndarray, max_shift: int = 2
 ):
+    n_instances, n_dims, n_timepoints = X.shape
     optimal_shifts = np.zeros_like(X)
 
     for i in range(X.shape[0]):
@@ -20,29 +20,25 @@ def shift_invariant_average(
             _, curr_shift = shift_invariant_best_shift(initial_center, X[i], max_shift)
             optimal_shifts[i] = curr_shift
 
-    new_center = np.zeros_like(initial_center)
-    n_cases, n_dims, n_timepoints = X.shape
-
     if optimal_shifts.shape[0] == 0:
-        return new_center
+        return np.zeros((n_dims, n_timepoints))
 
-    for d_i in range(n_dims):
-        a_di = optimal_shifts[:, d_i, :]
-        b = a_di / np.tile(norm(a_di, axis=1), (n_timepoints, 1)).T
-        M = np.matmul(b.T, b) - (n_cases + 1) * np.eye(n_timepoints)
+    normalized_shifts = optimal_shifts / np.sqrt(
+        np.sum(optimal_shifts**2, axis=2)
+    ).reshape(n_instances, n_dims, 1)
 
-        w, v = eigh(M)
-        ksc_di = v[:, np.argmax(w)]
+    M = np.zeros((n_dims, n_timepoints, n_timepoints))
+    for d in range(n_dims):
+        M[d] = np.dot(
+            normalized_shifts[:, d, :].T, normalized_shifts[:, d, :]
+        ) - n_instances * np.eye(n_timepoints)
 
-        dist1 = norm(a_di[0, :] - ksc_di.T)
-        dist2 = norm(a_di[0, :] - (-ksc_di.T))
+    new_center = np.zeros((n_dims, n_timepoints))
+    for d in range(n_dims):
+        eigvals, eigvecs = np.linalg.eig(M[d])
+        new_center[d] = np.real(eigvecs[:, np.argmax(eigvals)])
 
-        if dist1 > dist2:
-            ksc_di = -ksc_di
-
-        if np.sum(ksc_di) < 0:
-            ksc_di = -ksc_di
-
-        new_center[d_i, :] = ksc_di
+        if np.sum(new_center[d]) < 0:
+            new_center[d] = -new_center[d]
 
     return new_center